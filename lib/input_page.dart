10c5eb38f697e3c0655b6c3d2e8fbb0e112db9c2
Hemant Walia <hemant.walia.672@gmail.com>
2022-04-12 18:14:48 +0530

    Completed the 2 ToDo in build.gradle Added the SliderTheme to Slider Widget in input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -91,17 +91,27 @@ class _InputPageState extends State<InputPage> {
                           ),
                         ],
                       ),
-                      Slider(
-                        value: height.toDouble(),
-                        min: 120.0,
-                        max: 220.0,
-                        activeColor: Colors.pink,
-                        inactiveColor: Colors.white,
-                        onChanged: (double newvalue) {
-                          setState(() {
-                            height = newvalue.round();
-                          });
-                        },
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          thumbColor: Color(0xFFEB1555),
+                          activeTrackColor: Colors.white,
+                          inactiveTrackColor: Color(0xFF8D8E98),
+                          overlayColor: Color(0x29EB1555),
+                          overlayShape:
+                              RoundSliderOverlayShape(overlayRadius: 30.0),
+                          thumbShape:
+                              RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        ),
+                        child: Slider(
+                          value: height.toDouble(),
+                          min: 120.0,
+                          max: 220.0,
+                          onChanged: (double newvalue) {
+                            setState(() {
+                              height = newvalue.round();
+                            });
+                          },
+                        ),
                       ),
                     ],
                   ),
