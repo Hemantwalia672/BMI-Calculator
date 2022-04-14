dcc94ddde9c885e9cb3f7fb4a760203a9e603609
Hemant Walia <hemant.walia.672@gmail.com>
2022-04-14 16:54:07 +0530

    Designed the Results page

---
+++ lib/screens/results_page.dart
@@ -0,0 +1,72 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:bmi_calculator/components/bottom_button.dart';
+import 'package:bmi_calculator/components/card_one.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF0A0E21),
+          title: Center(child: Text('BMI CALCULATOR')),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // The Upper Text
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(15.0),
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  ' Your Result',
+                  style: kTitleTextStyle,
+                ),
+              ),
+            ),
+            // Result Area
+            Expanded(
+              flex: 5,
+              child: CardOne(
+                  colour: kActiveCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Normal',
+                        style: kResultTextStyle,
+                      ),
+                      Text(
+                        '18.3',
+                        style: kBmiTextStyle,
+                      ),
+                      Text(
+                        'BMI Description',
+                        style: kBodyTextStyle,
+                      )
+                    ],
+                  ),
+                  onPress: () {}),
+            ),
+            BottomButton(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                buttonTitle: 'RE-CALCULATE')
+          ],
+        ),
+      ),
+    );
+  }
+}
